f5f19487cb5587bfc6ae70840964eb2e5fc39b40
palgang <palgangmori@gmail.com>
2020-10-23 21:49:49 +0200

    Update main.tf
    Allowing port 80 again

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -22,10 +22,10 @@ resource "google_compute_firewall" "allow-http" {
   network = "${local.network}"
   project = "${var.project}"
 
-#  allow {
-#    protocol = "tcp"
-#    ports    = ["80"]
-#  }
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
 
   target_tags   = ["http-server"]
   source_ranges = ["0.0.0.0/0"]
